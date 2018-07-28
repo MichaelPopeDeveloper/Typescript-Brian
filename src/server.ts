0be32f4929768404c1df1840ca833b616125918e
michaelpopedeveloper <michaelpopedeveloper@gmail.com>
2018-07-28 09:29:48 -0700

    First Commit

---
+++ src/server.ts
@@ -0,0 +1,124 @@
+import * as bodyParser from "body-parser";
+import * as cookieParser from "cookie-parser";
+import * as express from "express";
+import * as logger from "morgan";
+import * as path from "path";
+import errorHandler = require("errorhandler");
+import methodOverride = require("errorhandler");
+import { IndexRoute } from "./routes/index";
+
+/**
+ * The server
+ * 
+ * @class Server
+ * 
+ */
+export class Server {
+
+    public app: express.Application;
+
+    /**
+     * Bootstrap the application.
+     * @class Server
+     * @method Bootstrap
+     * @static
+     * @return {ng.auto.IInjectorService} Returns the newly created injector for this app.
+     */
+    public static bootstrap(): Server {
+        return new Server();
+    }
+
+    /**
+     * Constructor.
+     * 
+     * @class Server
+     * @constructor
+     */
+    constructor() {
+        //create expressjs application.
+        this.app = express();
+
+        //configure application.
+        this.config();
+
+        //add routes
+        this.routes();
+
+        //add api  
+        this.api();
+    }
+
+    /**
+     * Create REST API routes
+     * 
+     * @class Server
+     * @method api
+     */
+    public api() {
+        // empty for now
+    }
+
+    /**
+     * Configure application
+     * 
+     * @class Server
+     */
+    public config() {
+        // add static paths
+        this.app.use(express.static(path.join(__dirname, "public")));
+
+        //configure pug
+        this.app.set("views", path.join(__dirname, "views"));
+        this.app.set("view engine", "pug");
+
+        //use logger middleware
+        this.app.use(logger("dev"));
+
+        //use json form parser middleware
+        this.app.use(bodyParser.json());
+
+        //use query string parser middleware
+        this.app.use(bodyParser.urlencoded({
+            extended: true
+        }));
+
+        //use cookie parser middleware
+        this.app.use(cookieParser("SECRET_GOES_HERE"));
+
+        //use override middleware
+        this.app.use(methodOverride());
+
+        //catch 404 and forward to error handler
+        this.app.use(function (err: any, req: express.Request, res: express.Response, next: express.NextFunction) {
+            err.status = 404;
+            next(err);
+        });
+
+        //error handling
+        this.app.use(errorHandler());
+    }
+
+    //public routes() {
+    //     // empty for now
+    // }
+
+    /**
+     * Create router
+     * 
+     * @class Server
+     * @method config
+     * @return void
+     */
+    private routes() {
+        let router: express.Router;
+        router = express.Router();
+
+        //IndexRoute
+        IndexRoute.create(router);
+
+        //use router middleware
+        this.app.use(router);
+    }
+
+
+}
